4dca9624736aa4cfebe4feb69021c03fbd39fabc
Toby O'Connell <tobyoconnell@hotmail.com>
2020-05-02 13:27:27 +0100

    Use int32_t in c header

--- Sources/CAdder/include/CAdder.h
+++ Sources/CAdder/include/CAdder.h
@@ -2,11 +2,17 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 typedef struct adder* adder_t;
 
 adder_t adder_construct();
 void adder_destruct(adder_t adder);
-int adder_add(adder_t adder, int a, int b);
+
+int adder_add(adder_t adder, int32_t a, int32_t b);
+
+typedef void (*adder_handler_t)(void* context, int32_t result);
+void adder_handler_add(adder_t adder, int32_t a, int32_t b, void* context, adder_handler_t handler);
 
 #ifdef __cplusplus
 }
